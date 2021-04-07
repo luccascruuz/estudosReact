a0de83f01aaa1ab88eac5e6352dc78b8d8c80478
Luccas Ribeiro <luccas_185@hotmail.com>
2021-04-07 15:09:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+color: green;
+`
+
+export function App() {
+  return (
+    <div className="App">
+      <Title>Hello Word</Title>
+    </div>
+  );
+}
+
